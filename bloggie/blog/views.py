fc5d23d3c249f4252ae2aa4e685b1980a342fd3d
Dama Michael Yohanna <get2dama11@gmail.com>
2023-02-15 13:03:42 +0100

    updated blog views. rewritten the detail view

--- bloggie/blog/views.py
+++ bloggie/blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 
+
 from .models import Post, Comment
+from .forms import CommentForm
 
 
 class BlogList(ListView):
@@ -10,18 +13,23 @@ class BlogList(ListView):
     context_object_name = 'post_list'
 
 
-class BlogView(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-    context_object_name = 'post_detail'
-
-    def get_context_data(self, **kwargs):
-        context = super(BlogView, self).get_context_data(**kwargs)
-        context['more_post'] = Post.objects.all()[:2]
-        return context
-
-
-class PostComment(CreateView):
-    model = Comment
+class BlogView(CreateView):
     def post(self, request, *args, **kwargs):
+        post = Post.objects.get(slug=kwargs['slug'])
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            print("form is not valid")
+        messages.success(request, "Comment Submitted successfuly")
+        return redirect(f"/blog/{kwargs['slug']}")
+
+    def get(self, request, *args, **kwargs):
+        post = Post.objects.get(slug=kwargs['slug'])
+        more_post = Post.objects.all()[:2]
+        context ={'post_detail': post, 'more_post': more_post}
+        return render(request, 'post_detail.html', context)
+
 
