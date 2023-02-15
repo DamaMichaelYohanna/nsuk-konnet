ad570f15dbef5436192c8dbd124e65dc8c55094f
Dama Michael Yohanna <get2dama11@gmail.com>
2023-02-15 13:03:18 +0100

    updated blog urls

--- bloggie/blog/urls.py
+++ bloggie/blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.BlogList.as_view(), name='blog list'),
     path("<slug:slug>/", views.BlogView.as_view(), name='blog_list'),
-    path("comment/", views.PostComment.as_view(), name="post comment"),
+    # path("comment/", views.PostComment.as_view(), name="post comment"),
 ]
