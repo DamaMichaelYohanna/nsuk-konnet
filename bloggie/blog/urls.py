a6a40d220888297b5e0c9e054693250b5d044df5
Dama Michael Yohanna <get2dama11@gmail.com>
2023-02-20 14:42:07 +0100

    update blog url and change url name

--- bloggie/blog/urls.py
+++ bloggie/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'blog'
 urlpatterns = [
-    path("", views.BlogList.as_view(), name='blog list'),
-    path("<slug:slug>/", views.BlogView.as_view(), name='blog_list'),
+    path("", views.BlogList.as_view(), name='post_list'),
+    path("<slug:slug>/", views.BlogView.as_view(), name='post_detail'),
     # path("comment/", views.PostComment.as_view(), name="post comment"),
 ]
