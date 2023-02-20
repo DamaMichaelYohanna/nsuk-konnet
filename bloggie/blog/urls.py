c6d45e87781a04326accea3356199c7854de6853
Dama Michael Yohanna <get2dama11@gmail.com>
2023-02-20 14:44:15 +0100

    update blog url: remove useless url

--- bloggie/blog/urls.py
+++ bloggie/blog/urls.py
@@ -5,5 +5,4 @@ app_name = 'blog'
 urlpatterns = [
     path("", views.BlogList.as_view(), name='post_list'),
     path("<slug:slug>/", views.BlogView.as_view(), name='post_detail'),
-    # path("comment/", views.PostComment.as_view(), name="post comment"),
 ]
