b0f41de4aabc3635d17368dbeb59f6a8a002a09a
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-13 22:40:38 +0100

    update market views

--- bloggie/market/views.py
+++ bloggie/market/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def market(request):
     context = {}
     return render(request, 'market.html', context)
+
+
+def add_product(request):
+    pass
+
+
+def edit_product(request):
+    pass
+
+
+def delete_product(reqeust):
+    pass
