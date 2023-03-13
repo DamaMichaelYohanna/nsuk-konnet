7542053a5bee7f8065ff7d428b0f0b6470b89443
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-13 22:40:25 +0100

    update market urls

--- bloggie/market/urls.py
+++ bloggie/market/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("", views.market, name='market place')
+    path("", views.market, name='market place'),
+    path("product/add", views.add_product, name="add product"),
+    path('product/update', views.edit_product, name='update product'),
+    path('product/delete', views.delete_product, name='delete product'),
 ]
