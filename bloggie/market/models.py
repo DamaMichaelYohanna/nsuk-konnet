814b9e14185d3b692ada460c98cc8ed20119be89
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-16 16:46:41 +0100

    updated database, added catalog and store

--- bloggie/market/models.py
+++ bloggie/market/models.py
@@ -10,9 +10,24 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     price = models.PositiveIntegerField()
-    location = models.CharField(max_length=100)
-    contact = models.CharField(max_length=12)
-    created_on = models.DateTimeField(auto_created=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.name} posted by {self.owner}'
+
+
+class Store(models.Model):
+    """model for stores/shops """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name="store")
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    description = models.CharField(null=True, blank=True, max_length=200)
+    coordinate = models.CharField(default="(0, 0)", max_length=20)
+    rating = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Catalog(models.Model):
+    """catalogue for none store owner to publish their goods"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    goods = models.ManyToManyField(Product)
