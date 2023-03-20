eff1afb3973c51135a47b65ecf540a1246798794
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-20 18:05:35 +0100

    added place order url

--- bloggie/market/urls.py
+++ bloggie/market/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns =[
     path("", views.market, name='market place'),
+    path("order/place/<int:pk>", views.place_order,  name="place_order"),
     path("product/add", views.add_product, name="add product"),
     path('product/update', views.edit_product, name='update product'),
     path('product/delete', views.delete_product, name='delete product'),
