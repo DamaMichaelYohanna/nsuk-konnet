bca11b5ecbed0de27ee17063c671eb86d0d83e29
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-20 18:04:18 +0100

    added order and storeorder to admin site

--- bloggie/market/admin.py
+++ bloggie/market/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Product, Store, Catalog
+from .models import Product, Store, Catalog, Order, StoreOrder
 
 admin.site.register(Product)
 admin.site.register(Store)
 admin.site.register(Catalog)
+admin.site.register(Order)
+admin.site.register(StoreOrder)
