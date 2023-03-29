5685ba60c4ad3c89dffa1787a7f3e96525ca7ec7
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-29 16:38:42 +0100

    added login view

--- bloggie/main/views.py
+++ bloggie/main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
 from blog.models import Post
+
+
+def login_view(request):
+    """custom login for user"""
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username, password)
+        if user and user.is_active:
+            login(request, user)
+            if user.store:
+                return redirect("/market/store")
+            elif user.catalog:
+                return redirect("/market/catalog")
+
+    return render(request, "main/login.html")
 
 
 def index(request):
@@ -12,7 +29,7 @@ def index(request):
 
 
 def about_us(request):
-    return render(request, 'about.html',)
+    return render(request, 'about.html', )
 
 
 def pass_questions(request):
