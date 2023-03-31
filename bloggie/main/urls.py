9d6cb385f89a0dd6db51d1806825db045765fee1
Dama Michael Yohanna <get2dama11@gmail.com>
2023-03-31 22:32:20 +0100

    updated url

--- bloggie/main/urls.py
+++ bloggie/main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     #     -----------------------------------
     path("login", views.login_view, name="login_view"),
     path("logout", views.logout_view, name="logout_view"),
+    path("register", views.register_view, name="register_view"),
 ]
