02617870856923880cdf0cbfdd2e4a31d7f981e9
Dama Michael Yohanna <get2dama11@gmail.com>
2023-05-10 15:29:30 +0100

    updated academic admin

--- bloggie/academic/admin.py
+++ bloggie/academic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from academic.models import Faculty, Department, Course, Level
+
+admin.site.register(Faculty)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Level)
