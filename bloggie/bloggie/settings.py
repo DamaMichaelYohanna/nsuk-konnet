186b16bf602237a4ae3415d83209703f9cb687ed
Dama Michael Yohanna <get2dama11@gmail.com>
2023-05-10 16:06:16 +0100

    updated settings

--- bloggie/bloggie/settings.py
+++ bloggie/bloggie/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'market', 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'academic', 'static'),
                     os.path.join(BASE_DIR, 'blog', 'static', ),
                     os.path.join(BASE_DIR, 'main', 'static'),
                     os.path.join(BASE_DIR, 'market', 'static'),
