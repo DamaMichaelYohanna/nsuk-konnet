b66fcf153d9a774639ccdc0302628877f0199d85
Dama Michael Yohanna <get2dama11@gmail.com>
2023-05-10 15:30:11 +0100

    updated academic views

--- bloggie/academic/views.py
+++ bloggie/academic/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from academic.models import Department, Faculty
+
+from academic.models import Course
 
 
 def course_list(request):
-    return render(request, "academic/course_list.html")
+    if request.method == "GET":
+
+        dept = request.GET.get("department")
+        level = request.GET.get("level")
+        if dept and level:
+            course = Course.objects.filter(department=dept, level=level)
+        else:
+            course = None
+    faculty = Faculty.objects.all()
+    context = {"faculty": faculty, 'courses': course}
+    return render(request, "academic/course_list.html", context)
+
+
+def load_department(request):
+    faculty_id = request.GET.get('faculty')
+    department = Department.objects.filter(faculty=faculty_id).order_by('name')
+    return render(request, 'academic/department_dropdown_list_options.html', {'dept': department})
