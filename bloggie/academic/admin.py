2b45dbc0aab87a9f1e78232d0d6270d8efa4ae11
Dama Michael Yohanna <get2dama11@gmail.com>
2023-05-30 12:17:53 +0100

    added borrowed course to admin site

--- bloggie/academic/admin.py
+++ bloggie/academic/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from academic.models import Faculty, Department, Course, Level
+from academic.models import Faculty, Department, Course, Level, BorrowedCourse
 
 admin.site.register(Faculty)
 admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Level)
+admin.site.register(BorrowedCourse)
