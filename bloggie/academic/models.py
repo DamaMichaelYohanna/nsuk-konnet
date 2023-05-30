cd48b1c2f516173f566b72a99fe7a7ccab9c2278
Dama Michael Yohanna <get2dama11@gmail.com>
2023-05-30 12:18:21 +0100

    added borrowed course model

--- bloggie/academic/models.py
+++ bloggie/academic/models.py
@@ -30,9 +30,21 @@ class Level(models.Model):
 
 class Course(models.Model):
     """model for different course with respect to their department """
-    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=7, null=True)
+    title = models.CharField(max_length=50)
+    pre = models.ForeignKey("Course", on_delete=models.DO_NOTHING, null=True, blank=True)
     department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
     level = models.ForeignKey(Level, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.title}"
+
+
+class BorrowedCourse(models.Model):
+    """Table for borrowed courses."""
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    level = models.ForeignKey(Level, on_delete=models.DO_NOTHING)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.course}'
