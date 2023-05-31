d9f82f36ea0453834b0225412bc487881abb22bf
Dama Michael Yohanna <get2dama11@gmail.com>
2023-06-01 00:30:56 +0100

    updated academic url

--- bloggie/academic/urls.py
+++ bloggie/academic/urls.py
@@ -3,8 +3,11 @@ from django.urls import  path
 from . import  views
 
 
-urlpatterns =[
+urlpatterns = [
     path("course/list", views.course_list, name="course list"),
     path('ajax/load-cities/', views.load_department, name='ajax_load_dept'),
+    path("orientation/", views.orientation, name="orientation"),
+    path("past-question/", views.past_question, name="pass questions"),
+    path("cbt/", views.cbt, name="cbt")
 
 ]
