bd75e3ca8478e1d01ba2388fa3b54264a9d15c10
Dama Michael Yohanna <get2dama11@gmail.com>
2023-10-17 23:17:57 +0100

    updated settings

--- bloggie/bloggie/settings.py
+++ bloggie/bloggie/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eg1pry7ph^u!dz8wklsbho&j5+tbgemjct2%7d*+v9d%#(g9dm'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
