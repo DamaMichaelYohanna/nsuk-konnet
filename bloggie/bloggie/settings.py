15b0da5525d5a4146a4ae7c6f121f671366d4f6e
Dama Michael Yohanna <get2dama11@gmail.com>
2023-10-18 00:01:50 +0100

    updated settings

--- bloggie/bloggie/settings.py
+++ bloggie/bloggie/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'bloggie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
     }
 }
 
