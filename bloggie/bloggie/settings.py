caad455369547933dbdca562c9ae2fe6b0db2fcb
Dama Michael Yohanna <get2dama11@gmail.com>
2023-10-18 15:15:16 +0100

    env changes

--- bloggie/bloggie/settings.py
+++ bloggie/bloggie/settings.py
@@ -9,14 +9,15 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os.path
 from pathlib import Path
-
+import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
-from dotenv import load_dotenv
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
